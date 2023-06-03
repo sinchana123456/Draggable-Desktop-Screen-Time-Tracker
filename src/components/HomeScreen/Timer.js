5e5b3c8736d81f61603f66a9c20918a187b0ece7
Sinchana Gowda <113234656+sinchana123456@users.noreply.github.com>
2023-06-03 17:27:53 +0530

    Google clone form

---
+++ src/components/HomeScreen/Timer.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import './timer.css';
+
+const Timer = () => {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds((prevSeconds) => prevSeconds + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+  };
+
+  return (
+    <div className="timer-container">
+      <h1 className="timer-title">Timer</h1>
+      <p>{formatTime(seconds)}</p>
+    </div>
+  );
+};
+
+export default Timer;
